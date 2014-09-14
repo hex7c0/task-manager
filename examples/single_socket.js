fb30bea04f2229abf7f325cb3e6669838325c6e5
hex7c0 <carniellifrancesco@gmail.com>
2014-09-14 12:54:36 +0200

    first examples

---
+++ examples/single_socket.js
@@ -0,0 +1,36 @@
+"use strict";
+/**
+ * @file socket example
+ * @module task-manager
+ * @package task-manager
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var task = require('../index.min.js'); // use require('task-manager')
+    var http = require('http');
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+/*
+ * use
+ */
+task('unix.sock');
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
