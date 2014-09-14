301e1220b23cc7e5f9c28e7046e007762bf10113
hex7c0 <carniellifrancesco@gmail.com>
2014-09-14 12:56:32 +0200

    task-manager

---
+++ index.js
@@ -0,0 +1,206 @@
+"use strict";
+/**
+ * @file task-manager main
+ * @module task-manager
+ * @package task-manager
+ * @subpackage main
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @copyright hex7c0 2014
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var cluster = require('cluster');
+    var unlink = require('fs').unlink;
+    var net = require('net');
+    var resolve = require('path').resolve;
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+/*
+ * functions
+ */
+/**
+ * function wrapper for multiple require
+ * 
+ * @function wrapper
+ * @param {Object} my - options
+ * @return {Object}
+ */
+function wrapper(my) {
+
+    /*
+     * closure
+     */
+    /**
+     * print to console
+     * 
+     * @param {String} console - output string
+     */
+    var output = function() {
+
+        return;
+    };
+    if (my.output) {
+        output = function(consolle) {
+
+            console.log(consolle);
+            return;
+        };
+    }
+
+    /**
+     * 'listening' listener
+     * 
+     * @param {Boolean} print - if print info
+     */
+    function start(print) {
+
+        server.listen(my.listen, function() {
+
+            if (print) {
+                output('task manager bound at ' + my.listen);
+            }
+            return;
+        });
+        return;
+    }
+
+    // 'connection' listener
+    var server = net.createServer(function(sock) {
+
+        var grant = false;
+        sock.on('end', function() {
+
+            if (my.auth) {
+                grant = false;
+            }
+            output('client disconnected');
+            return;
+        });
+        sock.on('data', function(buff) {
+
+            sock.pause();
+            var command = String(buff);
+
+            if (my.auth && grant == false) {
+                if (command.replace(/(\n)*(\r)*/g, '') === my.auth) {
+                    grant = true;
+                    sock.write('> hello master\n');
+                    output('client connected');
+                } else {
+                    output('client denied');
+                    sock.write('> auth required\n');
+                }
+                sock.resume();
+                return;
+            }
+
+            var temp = cluster.workers;
+            if (/^(kill|swap)/.test(command)) {
+                var pid = command.match(/[0-9]+\n/);
+                if (pid && (pid = Number(pid[0]))) {
+                    for ( var i in temp) {
+                        var index = temp[i];
+                        if (index.process.pid === pid) {
+                            index.kill();
+                            sock.write('> ' + pid + ' killed\n');
+                            break;
+                        }
+                    }
+                } else {
+                    var c = 0;
+                    for ( var i in temp) {
+                        temp[i].kill();
+                        c++;
+                    }
+                    sock.write('> ' + c + ' killed\n');
+                }
+            } else if (/^fork[\r]?\n/.test(command)) {
+                var pid = cluster.fork();
+                sock.write('> ' + pid.process.pid + ' forked\n');
+            } else if (/^ps[\r]?\n/.test(command)) {
+                var str = '> father pid: ' + process.pid + '\n';
+                for ( var i in temp) {
+                    str += '> child pid: ' + temp[i].process.pid + '\n';
+                }
+                sock.write(str);
+            } else if (/^(quit|exit)[\r]?\n$/.test(command)) {
+                sock.end('> bye\n');
+                process.exit(0);
+            } else {
+                sock.write('> unrecognized\n');
+            }
+            sock.resume();
+            return;
+        });
+
+        if (my.auth) {
+            if (grant) {
+                sock.write('> hello master\n');
+                output('client connected');
+            } else {
+                sock.write('> auth required\n');
+            }
+        } else {
+            sock.write('> hello master\n');
+        }
+        return;
+    });
+
+    server.on('error', function(e) {
+
+        if (e.code === 'EADDRINUSE') {
+            if (isNaN(my.listen)) {
+                unlink(my.listen, function(err) {
+
+                    if (err) {
+                        throw err;
+                    }
+                    start(false);
+                    return;
+                });
+            } else {
+                setTimeout(function() {
+
+                    start(false);
+                }, 1000);
+            }
+        }
+        return;
+    });
+
+    start(true);
+}
+
+/**
+ * options setting
+ * 
+ * @exports task
+ * @function task
+ * @param {Number|String} listen - listen type
+ * @param {Object} [options] - various options. Check README.md
+ * @return {Object}
+ */
+module.exports = function task(listen, options) {
+
+    if (!listen) {
+        throw new TypeError('listen required');
+    }
+    var what = Number(listen) || resolve(String(listen));
+
+    var options = options || Object.create(null);
+    var my = {
+        listen: what,
+        auth: options.auth ? String(options.auth) : false,
+        output: options.output == false ? false : true
+    };
+    return wrapper(my);
+};
