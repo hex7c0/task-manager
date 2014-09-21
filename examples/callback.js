472a95dcfabbdb3aa4a57238661a09ec7e89900c
hex7c0 <carniellifrancesco@gmail.com>
2014-09-21 19:38:18 +0200

    callback example

---
+++ examples/callback.js
@@ -0,0 +1,57 @@
+"use strict";
+/**
+ * @file callback example
+ * @module task-manager
+ * @package task-manager
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var task = require('../index.min.js'); // use require('task-manager')
+    var cluster = require('cluster');
+    var http = require('http');
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+/*
+ * use
+ */
+if (cluster.isMaster) {
+    // 2 children
+    cluster.fork();
+    cluster.fork();
+    cluster.on('exit', function(worker, code, signal) {
+
+        console.error(worker.process.pid + ' died');
+        return;
+    });
+
+    task('unix.sock', {
+        custom: /^parrot/,
+        callback: function(sock, command) {
+
+            sock.write(command);
+            return;
+        }
+    });
+}
+
+if (cluster.isWorker) {
+    http.createServer(function(req, res) {
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Hello World\n');
+    }).listen(3000, '127.0.0.1');
+    console.log('Server running at http://127.0.0.1:3000/');
+}
