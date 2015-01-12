4d1b0e3fa39daf0c58700eb37d25ca3946945fc9
hex7c0 <carniellifrancesco@gmail.com>
2015-01-12 17:48:43 +0100

    (easy) debug minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,10 @@ module.exports = function(grunt) {
 
     uglify: {
       target: {
+        options: {
+          mangle: false,
+          beautify: true
+        },
         files: [ {
           expand: true,
           src: 'lib/**/*.js',
@@ -45,7 +49,8 @@ module.exports = function(grunt) {
         node: true,
         // relax
         laxbreak: true,
-        loopfunc: true
+        loopfunc: true,
+        shadow: true
       },
       target: {
         src: [ 'lib/**/*.js', 'module/**/*.js', 'index.js' ]
@@ -75,11 +80,12 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-endline');
 
   grunt.registerTask('lint', [ 'jshint' ]);
-  grunt.registerTask('min', [ 'clean', 'endline' ]);
+  grunt.registerTask('min', [ 'clean', 'uglify', 'endline' ]);
   grunt.registerTask('default', [ 'lint', 'min' ]);
 
   return;
